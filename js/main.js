75a93da19fa03ee35d5826dcf5682100c967c6ae
unknown <leeminho03072001@gmail.com>
2022-07-27 17:23:42 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.navbar__links');
+const menuBtn = document.querySelector('.navbar__icons');
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('navbar__open');
+    menuBtn.classList.toggle('open');
+})
